30159f470a71b9b2dcd7c5098496eac808182c9b
PixelEdition <70499960+PixelEdition@users.noreply.github.com>
2022-07-26 10:03:13 +0700

    e

---
+++ timetable/assets/js/components/settings.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+    if (localStorage.getItem("theme")) {
+        document.getElementById("theme-select").value = localStorage.getItem("theme")
+    } if (!localStorage.getItem("theme")) {
+        document.getElementById("theme-select").value = "sync-os"
+    }
+    
+})
+
+function settingsIgnore() {
+    document.getElementById("settings-wrapper").style.display = "none"
+}
+
+function settingsDone() {
+    document.getElementById("settings-wrapper").style.display = "none"
+
+    if (document.getElementById("enable-timeremaining").checked == true) {
+        localStorage.setItem("enableTimeRemaining", true)
+        if (document.getElementById("enable-timeremaining-sound").checked == true) {
+            localStorage.setItem("enableTimeRemainingSound", true)
+        } if (document.getElementById("enable-timeremaining-sound").checked == false) {
+            localStorage.setItem("enableTimeRemainingSound", false)
+        }
+    } if (document.getElementById("enable-timeremaining").checked == false) {
+        localStorage.setItem("enableTimeRemaining", false)
+        localStorage.setItem("enableTimeRemainingSound", false)
+    }
+
+    if (localStorage.getItem('theme') != document.getElementById("theme-select").value) {
+        localStorage.setItem('theme', document.getElementById("theme-select").value)
+        location.reload()
+    }
+}
