ea88756b81c7e7f19c7b29e667205db44654d00a
PixelEdition <pixel.pxed@gmail.com>
2022-08-20 20:01:52 +0700

    Timetable v3.4.4

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,6 +1,6 @@
 // Basic Timetable information.
 var copyrightyear = "2022"
-var timetableversion = "3.4.3"
+var timetableversion = "3.4.4"
 
 // Elective classe (Is disabled in custom classes, since the user 
 // would put their data in by themselves, not requiring the feature.)
