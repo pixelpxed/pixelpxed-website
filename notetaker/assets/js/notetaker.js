9b5b6a655d55d3a1a371a68f17c4b5620e0ec1e6
PixelEdition <pixel.pxed@gmail.com>
2022-09-22 12:23:56 +0700

    Overall Website Update
    - Notetaker: notetaker.js rewrite
    - Global: global.css added table

--- notetaker/assets/js/notetaker.js
+++ notetaker/assets/js/notetaker.js
@@ -1,69 +1,191 @@
-// Dear anyone who might be reading this code...
-// I don't know what I wrote also, thank you,
+var notesData
+
+let typingTimeout
+const saveIndicator = document.querySelector(".save-indicator")
+
+const elementFilePath = document.querySelector(".main-edit-filepath")
+const elementTitle = document.querySelector(".main-edit-title")
+const elementContent = document.querySelector(".main-edit-content")
+
+const fullPageOverlay = document.querySelector(".full-page-overlay")
+
+const parameters = new URLSearchParams(window.location.search);
+var currentNoteId = parseInt(parameters.get("noteid") ?? 0)
 
 window.addEventListener("load", () => {
+    // Log warning message when using console.
     console.log("%cWarning!%c\n\nConsole is a very powerful tool.\n\nRunning codes you don't understand can break your Notetaker, or worse, steal your personal data.\n\nNever paste and run any code you don't understand.",
         "font-family: sans-serif; font-weight: bold; font-size: 1.5em; color: var(--color-red);",
         "font-family: sans-serif;")
 
     loadNote()
     settings()
-
-    document.querySelector(".full-page-overlay").style.display = "none";
-
-    if (!localStorage.getItem("theme")) {
-        localStorage.setItem("theme", "sync-os");
-    }
-    document.querySelector("html").setAttribute("theme", localStorage.getItem("theme"));
+    
+    // Remove the full page loading blur
+    fullPageOverlay.style.display = "none";
 });
 
-var notes = [
-    {
-        "title": "Something went wrong...",
-        "content": "We're unable to load your note, please try again later. 🙏<br><br>Error: Timed out while fetching note."
-    }
-]
+// Multiuse code.
+function saveNoteToLocalStorage() {
+    localStorage.setItem("notetaker-notesData", JSON.stringify(notesData))
+}
 
-// Universal already-existing popup toggle
-function togglePopup(element) {
-    const elementSelected = document.querySelector(element)
-    const fullPageOverlay = document.querySelector(".full-page-overlay")
+// Compatibility function.
+function toggleSidebar() {
+    toggleSidebarView()
+} function togglePopup(element) {
+    toggleExistingPopup(element)
+}
 
-    if (elementSelected.style.display == "block") {
+// Toggling sidebar view.
+function toggleSidebarView() {
+    document.querySelector(".side-bar").classList.toggle("side-bar-hide")
+    document.querySelector(".main-content").classList.toggle("col-span-2")
+}
+
+function toggleExistingPopup(element) {
+    // Element definition
+    const selectedElement = document.querySelector(element)
+
+    // If element is not displayed, display as block, else display as none.
+    if (selectedElement.style.display == "block") {
         fullPageOverlay.style.display = "none"
-        return elementSelected.style.display = "none"
-    } if (elementSelected.style.display != "block") {
+        return selectedElement.style.display = "none"
+    } if (selectedElement.style.display != "block") {
         fullPageOverlay.style.display = "block"
-        return elementSelected.style.display = "block"
+        return selectedElement.style.display = "block"
     }
 }
 
-// Toggle sidebar element
-function toggleSidebar() {
-    document.querySelector(".side-bar").classList.toggle("side-bar-hide")
-    document.querySelector(".main-content").classList.toggle("span-col-2")
+function loadNote() {
+    function insertNotesData() {
+        // If there's no note in JSON list, create a new note.
+        if (notesData.length === 0) {
+            newNote()
+        }
+        // If note ID requested is higher than current note list count, redirects to note ID 0.
+        if (currentNoteId > (notesData.length - 1)) {
+            location.href = `?noteid=0`
+        }
+
+        elementFilePath.innerHTML = notesData[currentNoteId].title
+        elementTitle.innerHTML = notesData[currentNoteId].title
+        elementContent.innerHTML = notesData[currentNoteId].content
+
+        for (let i = 0; i < notesData.length; i++) {
+            if (i == currentNoteId) {
+                document.querySelector(".notes-panel").insertAdjacentHTML("beforeend", `
+                    <li class="notes-list notes-list-active" noteid="${i}">
+                        <a class="notes-list-${i}" href="?noteid=${i}">${notesData[i].title}</a>
+                        <a class="material-symbols-outlined" onclick="deleteNote(${i})">delete</a>
+                    </li>
+                `)
+            } if (i != currentNoteId) {
+                document.querySelector(".notes-panel").insertAdjacentHTML("beforeend", `
+                    <li class="notes-list" noteid="${i}">
+                        <a class="notes-list-${i}" href="?noteid=${i}">${notesData[i].title}</a>
+                        <a class="material-symbols-outlined" onclick="deleteNote(${i})">delete</a>
+                    </li>
+                `)
+            }
+        }
+    }
+
+    // First time running
+    if (localStorage.getItem("notetaker-firstRun") === null) {
+        // Load template note from file templatenote.json
+        fetch("./assets/json/templatenote.json")
+            .then((res) => res.json())
+            .then((data) => {
+                notesData = data
+
+                localStorage.setItem("notetaker-notesData", JSON.stringify(notesData))
+                localStorage.setItem("notetaker-firstRun", false)
+
+                insertNotesData()
+            })
+    }
+    // Normal note loading
+    if (localStorage.getItem("notetaker-firstRun") === "false") {
+        notesData = JSON.parse(localStorage.getItem("notetaker-notesData"))
+        insertNotesData()
+    }
+}
+
+// If user stop typing in editor for 5 seconds, save the note.
+// But also saves note every 60 seconds (prevents data lost from crashes.)
+elementTitle.addEventListener("keyup", typingTimeoutSave);
+elementContent.addEventListener("keyup", typingTimeoutSave);
+function typingTimeoutSave() {
+    // Do not display save indicator whe typing.
+    saveIndicator.style.display = "none"
+
+    // Clear existing typing timeout to start clock again.
+    clearTimeout(typingTimeout)
+    typingTimeout = setTimeout(saveNote, 5000);
+
+    setTimeout(saveNote, 60000)
+}
+
+function saveNote() {
+    // Indicate note has been saved.
+    saveIndicator.style.display = "inline-flex"
+
+    // Saves the new content to notesData list.
+    notesData[currentNoteId].title = elementTitle.innerHTML
+    notesData[currentNoteId].content = elementContent.innerHTML
+
+    // Change UI note title in notes list and file path bar.
+    document.querySelector(`.notes-list-${currentNoteId}`).innerHTML = elementTitle.innerHTML
+    elementFilePath.innerHTML = elementTitle.innerHTML
+
+    // Saves note to localStorage
+    saveNoteToLocalStorage()
 }
 
 function newNote() {
-    notes.push({"title": "New Note", "content": "Click here to start editing your new note."})
-    localStorage.setItem("notetaker-notesData", JSON.stringify(notes))
-    location.href = `?noteid=${(notes.length) - 1}`
+    // Add new a new note template to notesData list
+    notesData.push(
+        {
+            "title": "New Note",
+            "content": "Click here to start editing your new note."
+        }
+    )
+
+    // Save note to localStorage
+    saveNoteToLocalStorage()
+
+    // Redirects to new note.
+    location.href = `?noteid=${notesData.length - 1}`
 }
 
-function deleteNote(noteid) {
-    var amountDel = noteid
-    if (amountDel === 0) {
-        amountDel = 1
-    }
+// Delete a note.
+function deleteNote(id) {
+    // Delete note, save new note data, then remove the note deleted from sidebar list.
+    notesData.splice(id, 1)
 
-    notes.splice(noteid, amountDel)
-    localStorage.setItem("notetaker-notesData", JSON.stringify(notes))
-    document.querySelector(`li[noteid="${noteid}"]`).remove()
-    location.href = `?noteid=${(notes.length) - 1}`
+    saveNoteToLocalStorage()
+
+    document.querySelector(`li[noteid="${id}"]`).remove()
+
+    // If user deletes the current note, redirects to the first note.
+    if (currentNoteId == id) {
+        return location.href = `?noteid=0`
+    }
+    // If user's current note id is higher than the deleted note, 
+    // redirects to the id for the current note.
+    if (currentNoteId > id) {
+        return location.href = `?noteid=${currentNoteId - 1}`
+    }
 }
 
 // Settings
 function settings() {
+    // Theme
+    if (!localStorage.getItem("theme")) {
+        localStorage.setItem("theme", "sync-os");
+    } document.querySelector("html").setAttribute("theme", localStorage.getItem("theme"));
+    
     document.querySelectorAll("input[name='settings-theme']").forEach((element) => {
         if (element.value == localStorage.getItem("theme")) {
             element.setAttribute("checked", "checked")
@@ -101,95 +223,4 @@ function settings() {
             }
         })
     })
-}
-
-// Notetaking loading
-function loadNote() {
-    const elementFilePath = document.querySelector(".main-edit-filepath")
-    const elementTitle = document.querySelector(".main-edit-title")
-    const elementContent = document.querySelector(".main-edit-content")
-
-    const parameters = new URLSearchParams(window.location.search);
-    var noteid = parseInt(parameters.get("noteid") ?? 0)
-
-    if (noteid === "") {
-        noteid = 0
-        notes = [
-            {
-                "title": "Click on a note to start editing.",
-                "content": "It's that easy!"
-            }
-        ]
-        return insertContent()
-    }
-    
-    // Private function, insert notes content.
-    function insertContent() {
-        elementFilePath.innerHTML = notes[noteid].title
-        elementTitle.innerHTML = notes[noteid].title
-        elementContent.innerHTML = notes[noteid].content
-    }
-
-    function insertNotesPanel() {
-        for (let i = 0; i < notes.length; i++) {
-            document.querySelector(".notes-panel").insertAdjacentHTML("beforeend", `
-                <li noteid=${i} class="notes-list">
-                    <a class="notes-list-${i}" href="?noteid=${i}">${notes[i].title}</a>
-                    <a onclick="deleteNote(${i})" class="material-symbols-outlined">delete</a>
-                </li>
-            `)
-        }
-    }
-
-    function insertNotesHandler() {
-        notes = JSON.parse(localStorage.getItem("notetaker-notesData"))
-    
-        if (notes.length === 0) {
-            newNote()
-        } if (noteid > (notes.length - 1)) {
-            location.href = "?noteid=0"
-        }
-
-        insertContent()
-        insertNotesPanel()
-    }
-
-    if (localStorage.getItem("notetaker-firstRun") === null) {
-        // Load the note
-        fetch("./assets/json/templatenote.json")
-            .then((res) => res.json())
-            .then((content) => {
-                localStorage.setItem("notetaker-notesData", JSON.stringify(content))
-                localStorage.setItem("notetaker-firstRun", false)
-
-                insertNotesHandler()
-            })
-        console.log("Notetaker detected that this session is your first time.")
-    } if (localStorage.getItem("notetaker-firstRun") === "false") {
-        insertNotesHandler()
-    }
-
-    let typingTimeout;
-
-    elementTitle.addEventListener("keyup", typingTimeoutSave);
-    elementContent.addEventListener("keyup", typingTimeoutSave);
-    
-    function typingTimeoutSave() {
-        document.querySelector(".save-indicator").style.display = "none"
-        clearTimeout(typingTimeout);
-        typingTimeout = setTimeout(saveNote, 5000)
-    }
-
-    function saveNote() {
-        document.querySelector(".save-indicator").style.display = "inline-flex"
-        
-        notes[noteid].title = elementTitle.innerHTML
-        notes[noteid].content = elementContent.innerHTML
-
-        localStorage.setItem("notetaker-notesData", JSON.stringify(notes))
-
-        document.querySelector(`.notes-list-${noteid}`).innerHTML = elementTitle.innerHTML
-        
-        elementFilePath.innerHTML = elementTitle.innerHTML
-    }
 }
