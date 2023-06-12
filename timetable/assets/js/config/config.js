92a13cf1ed562acb122a9cada887c6d1b3d1f327
PixelEdition <pixel.pxed@gmail.com>
2023-06-12 22:39:39 +0700

    Timetable v3.8.3, Global
    Timetable v3.8.3
    - Updated to-do table layout.
    
    Global
    - Updated table design language slightly to have an odd/even row color.

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.8.2"
+var timetableversion = "3.8.3"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
