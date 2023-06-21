c7da4fa21abdf2b82dedff45809816b208b95305
PixelEdition <70499960+PixelEdition@users.noreply.github.com>
2023-06-21 22:54:00 +0700

    Timetable v3.8.8a

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.8.8"
+var timetableversion = "3.8.8a"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
