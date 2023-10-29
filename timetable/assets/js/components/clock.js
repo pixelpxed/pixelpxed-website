f72a6fc788c34b06dfa1bc749e5ebc3c18527374
PixelEdition <pixel.pxed@gmail.com>
2023-10-30 02:19:16 +0700

    Timetable v3.15.0: The Dynamic Timetable
    Timetable v3.15.0
     - Added week dates in grid days.
     - 'DClass' and 'AllDay' has been replaced with '-extend' flag, will merge with the grid before.
     - 'Add Work' in to-do popup now redirects to pawin.tk.
     - Moved time to navigation bar instead in PWA.
     - Added class overrides, overridden classes will show up as a darker/lighter border.
     - Timetable files update.
     - Minor fixes and changes.

--- timetable/assets/js/components/clock.js
+++ timetable/assets/js/components/clock.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     clock()
     setMainClock()
+    calculateDaysInWeek()
 });
 
 var daylist = [
@@ -38,6 +39,21 @@ var monthlist = [
     "December"
 ]
 
+var shortmonthlist = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+]
+
 // Set Public Variables
 var today = undefined;
 var day = undefined;
@@ -58,6 +74,9 @@ function clock() {
         month = monthlist[today.getMonth()];
         year = today.getFullYear();
 
+        shortday = shortdaylist[today.getDay()];
+        shortmonth = shortmonthlist[today.getMonth()];
+        
         hour = today.getHours();
         minute = today.getMinutes();
         second = today.getSeconds();
@@ -65,8 +84,43 @@ function clock() {
 }
 
 function setMainClock() {
-    setInterval(() => {
-        document.getElementById("clock").innerHTML = `${hour > 9 ? hour : "0" + hour}:${minute >= 10 ? minute : "0" + minute}:${second >= 10 ? second : "0" + second}`
-        document.getElementById("date").innerHTML = `${day}, ${date > 9 ? date : "0" + date} ${month} ${year}`;
-    }, 1000);
+    if (!window.matchMedia('(display-mode: standalone)').matches) {
+        setInterval(() => {
+            document.getElementById("clock").innerHTML = `${hour > 9 ? hour : "0" + hour}:${minute >= 10 ? minute : "0" + minute}:${second >= 10 ? second : "0" + second}`
+            document.getElementById("date").innerHTML = `${day}, ${date > 9 ? date : "0" + date} ${month} ${year}`;
+        }, 1000);
+    }
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+        document.querySelector(".nav-pwa-timetable").innerHTML = `---, -- --- ---- / --:--:--`;
+        setInterval(() => {
+            document.querySelector(".nav-pwa-timetable").innerHTML = `${shortday}, ${date > 9 ? date : "0" + date} ${shortmonth} ${year} / ${hour > 9 ? hour : "0" + hour}:${minute >= 10 ? minute : "0" + minute}:${second >= 10 ? second : "0" + second}`;
+        }, 1000);
+    }
+}
+
+var curtime = new Date()
+var datesInWeek = undefined
+function calculateDaysInWeek() {
+    // Modified from: https://stackoverflow.com/questions/43008354/get-all-days-of-the-week-given-a-day
+
+    datesInWeek = new Array(); 
+    curtime.setDate((curtime.getDate() - curtime.getDay()));
+    for (var i = 0; i < 7; i++) {
+        datesInWeek.push(
+            new Date(curtime)
+        ); 
+        curtime.setDate(curtime.getDate() + 1);
+    }
+
+    // Set the dates into the grid
+
+    for (let i = 0; i < 5; i++) {
+        var weekNow = datesInWeek[i + 1];
+
+        var weekMonth = shortmonthlist[weekNow.getMonth()];
+        var weekDate = weekNow.getDate();
+        var weekYear = weekNow.getFullYear();
+
+        document.getElementById(`date${i}`).innerHTML = `${weekMonth} ${weekDate > 9 ? weekDate : "0" + weekDate}, ${weekYear}`
+    }
 }
