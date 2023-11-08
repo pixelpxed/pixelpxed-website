8c9e3d98fed8c156a0510e39563072a0dc992057
PixelEdition <pixel.pxed@gmail.com>
2023-11-08 19:54:37 +0700

    Timetable v3.16.0d

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.16.0c"
+var timetableversion = "3.16.0d"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
