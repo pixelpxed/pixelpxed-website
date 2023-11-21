be9c91e6c066e76b01af18360f0d30f863814d4f
PixelEdition <70499960+PixelEdition@users.noreply.github.com>
2023-11-21 11:36:51 +0700

    Timetable v3.17.0, Notetaker v1.16.0

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.17.0-beta"
+var timetableversion = "3.17.0"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
