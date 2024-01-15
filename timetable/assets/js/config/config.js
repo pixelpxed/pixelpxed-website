b7bc589b0c42b0dd0767b3b63c133e5234c60a01
PixelEdition <pixel.pxed@gmail.com>
2024-01-15 12:25:42 +0700

    Timetable v3.18.2ea-sports

--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.18.2d"
+var timetableversion = "3.18.2ea-sports"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
