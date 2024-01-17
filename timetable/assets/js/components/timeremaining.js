0701928e85b5bdf8a67fb9448d0b601144f58001
PixelEdition <pixel.pxed@gmail.com>
2024-01-17 13:04:50 +0700

    Timetable v3.20.2

--- timetable/assets/js/components/timeremaining.js
+++ timetable/assets/js/components/timeremaining.js
@@ -7,86 +7,88 @@ var timeleftinterval = undefined
 var bellChimes = new Audio("/timetable/assets/sound/bellChimes.mp3")
 
 function timeRemaining() {
-    // Inserts the HTML code for time remaining after clock.
-    document.querySelector(".clock-wrapper").insertAdjacentHTML("afterend", `
-        <p class="timeremaining">
-            <b style='font-weight: var(--font-weight-semibold);'><span class="timeremaining-subj">-------</span></b>, <span id="timeremaining-refreshcontent">--:--</span>&nbsp;remaining
-        </p>
-    `)
-
-    // Set variables.
-    var periodTimeLeft = classtimes[classtime_type]["timeremaining"];
-    var periodperday = periodTimeLeft["periodperday"];
-    var periodlength = periodTimeLeft["periodlength"];
-    var starthour = periodTimeLeft["starthour"];
-    var startmin = periodTimeLeft["startmin"];
-
-    // Calculated every 1 second.
-    timeleftinterval = setInterval(() => {
-        var curtimeM = (hour * 60) + minute;
-        var curtimeS = (curtimeM * 60) + second;
-
-        var endtime = ((starthour * 60) * 60) + (startmin * 60);
-        var timediff = endtime - curtimeS;
-
-        var periodno = -1;
-
-        while (timediff < 0) {
-            var timediff = timediff + (periodlength * 60);
-            var periodno = periodno + 1;
-        }
-
-        var rseconds = timediff % 60;
-        var rminutes = Math.floor(timediff / 60);
-
-        if (periodno >= 0 && periodno <= periodperday) {
-            // Check if period ends (hour and minutes = 0)
-            if ((rminutes == 0) && (rseconds == 0)) {
-                // If sound setting is enabled, play the sound.
-                if (localStorage.getItem("timetable-enableTimeRemainingSound") === "true") {
-                    bellChimes.play();
-                }
-
-                clearActiveClass()
+    setTimeout(() => {
+        // Inserts the HTML code for time remaining after clock.
+        document.querySelector(".clock-wrapper").insertAdjacentHTML("afterend", `
+            <p class="timeremaining">
+                <b style='font-weight: var(--font-weight-semibold);'><span class="timeremaining-subj">-------</span></b>, <span id="timeremaining-refreshcontent">--:--</span>&nbsp;remaining
+            </p>
+        `)
+        
+        // Set variables.
+        var periodTimeLeft = classtimes[classtime_type]["timeremaining"];
+        var periodperday = periodTimeLeft["periodperday"];
+        var periodlength = periodTimeLeft["periodlength"];
+        var starthour = periodTimeLeft["starthour"];
+        var startmin = periodTimeLeft["startmin"];
+        
+        // Calculated every 1 second.
+        timeleftinterval = setInterval(() => {
+            var curtimeM = (hour * 60) + minute;
+            var curtimeS = (curtimeM * 60) + second;
+        
+            var endtime = ((starthour * 60) * 60) + (startmin * 60);
+            var timediff = endtime - curtimeS;
+        
+            var periodno = -1;
+        
+            while (timediff < 0) {
+                var timediff = timediff + (periodlength * 60);
+                var periodno = periodno + 1;
             }
-
-            if (localStorage.getItem("timetable-currentClassHighlight") === "true") {
-                var activeGridId = (today.getDay() * 11) + periodno
-
-                if (classes[activeGridId + 1] == "-extend") {
-                    var rminutesextend = 0
-                    for (let i = 1; classes[activeGridId + (i + 1)] == "-extend"; i++) {
-                        rminutesextend = (i) * 50
+        
+            var rseconds = timediff % 60;
+            var rminutes = Math.floor(timediff / 60);
+        
+            if (periodno >= 0 && periodno <= periodperday) {
+                // Check if period ends (hour and minutes = 0)
+                if ((rminutes == 0) && (rseconds == 0)) {
+                    // If sound setting is enabled, play the sound.
+                    if (localStorage.getItem("timetable-enableTimeRemainingSound") === "true") {
+                        bellChimes.play();
                     }
-                    rminutes = rminutes + (rminutesextend + 50)
+        
+                    clearActiveClass()
                 }
-                
-                if (classes[activeGridId] == "-extend") {
-                    for (let i = 1; classes[activeGridId - i] == "-extend"; i++) {
+        
+                if (localStorage.getItem("timetable-currentClassHighlight") === "true") {
+                    var activeGridId = (today.getDay() * 11) + periodno
+        
+                    if (classes[activeGridId + 1] == "-extend") {
+                        var rminutesextend = 0
+                        for (let i = 1; classes[activeGridId + (i + 1)] == "-extend"; i++) {
+                            rminutesextend = (i) * 50
+                        }
+                        rminutes = rminutes + (rminutesextend + 50)
+                    }
+                    
+                    if (classes[activeGridId] == "-extend") {
+                        for (let i = 1; classes[activeGridId - i] == "-extend"; i++) {
+                            activeGridId = activeGridId - 1
+                        }
                         activeGridId = activeGridId - 1
                     }
-                    activeGridId = activeGridId - 1
+                    
+                    if (activeGridId > 0) {
+                        document.getElementById(activeGridId).classList.remove("class-current")
+                    }
+                    document.getElementById(activeGridId + 1).classList.add("class-current")   
+                    document.querySelector(".timeremaining-subj").innerHTML = `${subj[document.getElementById(activeGridId + 1).innerHTML].subjname}`
+        
+                    console.log(activeGridId);
                 }
                 
-                if (activeGridId > 0) {
-                    document.getElementById(activeGridId).classList.remove("class-current")
-                }
-                document.getElementById(activeGridId + 1).classList.add("class-current")   
-                document.querySelector(".timeremaining-subj").innerHTML = `${subj[document.getElementById(activeGridId + 1).innerHTML].subjname}`
-
-                console.log(activeGridId);
+                document.getElementById("timeremaining-refreshcontent").innerHTML = `${rminutes >= 10 ? rminutes : "0" + rminutes}:${rseconds >= 10 ? rseconds : "0" + rseconds}`;
             }
-            
-            document.getElementById("timeremaining-refreshcontent").innerHTML = `${rminutes >= 10 ? rminutes : "0" + rminutes}:${rseconds >= 10 ? rseconds : "0" + rseconds}`;
-        }
-        if (periodno <= 0) {
-            document.querySelector(".timeremaining-subj").innerHTML = `School isn't started yet`
-            document.getElementById("timeremaining-refreshcontent").innerHTML = `--:--`;
-        }
-        if (periodno > periodperday) {
-            document.querySelector(".timeremaining-subj").innerHTML = `School has ended`
-            document.getElementById("timeremaining-refreshcontent").innerHTML = `--:--`;
-        }
+            if (periodno <= 0) {
+                document.querySelector(".timeremaining-subj").innerHTML = `School isn't started yet`
+                document.getElementById("timeremaining-refreshcontent").innerHTML = `--:--`;
+            }
+            if (periodno > periodperday) {
+                document.querySelector(".timeremaining-subj").innerHTML = `School has ended`
+                document.getElementById("timeremaining-refreshcontent").innerHTML = `--:--`;
+            }
+        }, 1000);
     }, 1000);
 }
 
--- timetable/assets/js/config/config.js
+++ timetable/assets/js/config/config.js
@@ -1,5 +1,5 @@
 // Basic Timetable information.
-var timetableversion = "3.20.1"
+var timetableversion = "3.20.2"
 
 // Timetable class fetch folder
 // Timetable will require a <classno>.json file in the folder
