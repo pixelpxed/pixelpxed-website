99924578a1a21cef9a135df46a29e16ab3ff61fe
pixelpxed <pixel.pxed@gmail.com>
2026-01-21 19:47:12 +0700

    feat: initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <title>pixelpxed</title>
+      </Head>
+      <body className="min-h-dvh antialiased">
         <Main />
         <NextScript />
       </body>
