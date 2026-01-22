85ed49d6ea75ed58e3d735e3b35ffe24898cc33c
pixelpxed <pixel.pxed@gmail.com>
2026-01-22 23:49:52 +0700

    feat: send / heading icon size

--- components/send/subcomponents/AboutHeader.tsx
+++ components/send/subcomponents/AboutHeader.tsx
@@ -4,7 +4,7 @@ const AboutHeader = () => {
   return (
     <div className="flex w-full items-center justify-between gap-2">
       <div className="flex items-center gap-1">
-        <MaterialIcon icon="package_2" />
+        <MaterialIcon icon="package_2" size={24} />
         <p className="font-bold">Send</p>
       </div>
     </div>
