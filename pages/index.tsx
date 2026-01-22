19919dd3c0ff1e19309dba8a0fe25ecfe9e4bc9c
pixelpxed <pixel.pxed@gmail.com>
2026-01-22 23:34:03 +0700

    feat: remove bloat pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,19 @@
+import { GetServerSideProps } from "next";
 import Button from "@/components/Button";
 import cn from "@/utils/helpers/cn";
 import Link from "next/link";
 
 const Home = () => {
-  return (
-    <>
-      {/* <div
-        className={cn(
-          "m-3 flex w-sm flex-col gap-2 rounded-lg border border-slate-200 p-3",
-        )}
-      >
-        <h1 className="text-xl font-medium">Design and Develop.</h1>
-        <div className="flex flex-col gap-1">
-          <p>
-            Undeniably lazy, self-taught developer. Focused on building with
-            web-technologies.
-          </p>
-          <p>
-            Also a minimalist UI/UX designer who values functionality as much as
-            aesthetics.
-          </p>
-          <p>
-            I’m Metawat Rojniweth, also known as @pixelpxed, nice to see you!
-          </p>
-        </div>
-        <Button>Hello, Metawat!</Button>
-      </div> */}
-      <div className="flex h-dvh w-dvw flex-col">
-        <div className="p-2">
-          <p>Landing is made-in-progress, so please wait.</p>
-          <p className="mb-2">
-            Apps built with new architecture (Next.js):{" "}
-            <Link href="/send" className="text-blue-500">
-              Send (App)
-            </Link>
-          </p>
-          <p>
-            <Link
-              href="https://pxedition.vercel.app/"
-              className="text-blue-500"
-            >
-              Old landing/site (Made with Vanilla):
-            </Link>
-          </p>
-        </div>
-        <iframe src="https://pxedition.vercel.app/" className="grow" />
-      </div>
-    </>
-  );
+  return <></>;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/send",
+      permanent: false,
+    },
+  };
 };
 
 export default Home;
