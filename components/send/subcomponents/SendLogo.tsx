3c807e69da6284633922b269afd2f4816547bc5e
pixelpxed <pixel.pxed@gmail.com>
2026-07-13 21:12:38 +0700

    feat: colorized sendlogo

--- components/send/subcomponents/SendLogo.tsx
+++ components/send/subcomponents/SendLogo.tsx
@@ -3,7 +3,7 @@ import MaterialIcon from "@/components/common/MaterialIcon";
 const SendLogo = () => {
   return (
     <div className="flex items-center gap-1">
-      <MaterialIcon icon="package_2" size={24} />
+      <MaterialIcon icon="package_2" size={24} className="text-primary" />
       <p className="font-bold">Send</p>
     </div>
   );
