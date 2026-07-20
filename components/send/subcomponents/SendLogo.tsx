bb1635dc1a893a8d370e23de5eacab892ecce420
pixelpxed <pixel.pxed@gmail.com>
2026-07-21 05:30:13 +0700

    feat(send): sendlogo acts as link to home

--- components/send/subcomponents/SendLogo.tsx
+++ components/send/subcomponents/SendLogo.tsx
@@ -1,11 +1,12 @@
 import MaterialIcon from "@/components/common/MaterialIcon";
+import Link from "next/link";
 
 const SendLogo = () => {
   return (
-    <div className="flex items-center gap-1">
+    <Link href="/send" className="flex items-center gap-1">
       <MaterialIcon icon="package_2" size={24} className="text-primary" />
       <p className="font-bold">Send</p>
-    </div>
+    </Link>
   );
 };
 
