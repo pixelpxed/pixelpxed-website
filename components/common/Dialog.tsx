d50bfaf31f4bd618f8474a0ff0890154455c2354
pixelpxed <pixel.pxed@gmail.com>
2026-07-23 19:28:16 +0700

    feat: Dialog supports customization

--- components/common/Dialog.tsx
+++ components/common/Dialog.tsx
@@ -6,11 +6,13 @@ const Dialog: StylableFC<{
   size?: "sm" | "md" | "lg";
   onClickOutside?: () => void;
   children: React.ReactNode;
-}> = ({ size = "sm", onClickOutside, children }) => {
+}> = ({ size = "sm", onClickOutside, children, className }) => {
   return (
     <motion.div
-      className="fixed top-0 left-0 z-1000 grid h-dvh w-dvw place-items-center
-        bg-black/50 p-3"
+      className={cn(
+        `fixed top-0 left-0 z-1000 grid h-dvh w-dvw place-items-center
+        bg-black/50 p-3`,
+      )}
       onClick={onClickOutside && onClickOutside}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -19,10 +21,11 @@ const Dialog: StylableFC<{
     >
       <motion.div
         className={cn(
-          "border-outline bg-background w-full rounded-lg border p-3",
+          "border-outline bg-surface-primary w-full rounded-lg border p-3",
           size == "sm" && "max-w-sm",
           size == "md" && "max-w-md",
           size == "lg" && "max-w-lg",
+          className,
         )}
         initial={{ scale: 1.1, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
